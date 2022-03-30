3e207d847b99ee41a207993faa80eaac14d09a82
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-03-30 11:14:54 -0300

    feat: add function to make matrix

---
+++ src/calc/index.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @param {number} lines Quantidade de linhas que a matriz terá
+ * @param {number} columns Quantidade de Colunas que a matriz terá
+ * @param {{ "l==c": string, "l!=c": string} | {"l==c": string, "l>c": string, "l<c": string} | {"l>=c": string, "l<c": string} | {"l>c": string, "l<=c": string}} rules Regras para a geração da matriz
+ * @returns {number[][]}
+ */
+
+export function make_matrix(lines, columns, rules) {
+    const matrix = []
+
+    for (let l = 1; l <= lines; l++) {
+        const line = []
+        for (let c = 1; c <= columns; c++) {
+            const command = Object.entries(rules).find(([condition]) => eval(condition))[1]
+            line.push(eval(command))
+        }
+        matrix.push(line)
+    }
+    return matrix
+}
