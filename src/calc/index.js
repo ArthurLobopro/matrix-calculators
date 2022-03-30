5b84e7829295ba09d2d2cc260790e5bf051cfaad
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-03-30 11:24:52 -0300

    feat: add function to subtract matrixes

--- src/calc/index.js
+++ src/calc/index.js
@@ -49,3 +49,33 @@ export function make_matrix(lines, columns, rules) {
         throw { message }
     }
 }
+
+/**
+ * 
+ * @param {number[][]} m1 
+ * @param {number[][]} m2 
+ * @returns {number[][]}
+ */
+ export function sub(m1, m2) {
+    const columns_m1 = m1[0].length
+    const lines_m1 = m1.length
+
+    const columns_m2 = m2[0].length
+    const lines_m2 = m2.length
+
+    if (columns_m1 === columns_m2 && lines_m1 === lines_m2) {
+        return m1.map((line, lineIndex) => {
+            return line.map((value, columnIndex) => {
+                return value - m2[lineIndex][columnIndex]
+            })
+        })
+    }else{
+        const hasEqualsLines = lines_m1 === lines_m2
+        const hasEqualsColumns = columns_m1 === columns_m2
+
+        const message = hasEqualsColumns && hasEqualsLines ? "As matrizes não têm o mesmo número de linhas e colunas" :
+            hasEqualsColumns ? "As matrizes não têm o mesmo número de colunas" : "As matrizes não têm o mesmo número de linhas"
+
+        throw { message }
+    }
+}
