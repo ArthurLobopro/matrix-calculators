78b2456946ccab4240dda7ecf37ed245d1d25720
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-04-23 15:17:14 -0300

    chore: update types

--- src/calc/types.ts
+++ src/calc/types.ts
@@ -1,3 +1,10 @@
 export type matrix = number[][]
 export type calcs_matrix = string[][]
-export type defaultReturns = { calcs: calcs_matrix, result: matrix }
+export type defaultReturns = { calcs: calcs_matrix, result: matrix } | undefined
+export type make_rules = (
+    { "l==c": string, "l!=c": string } |
+    { "l==c": string, "l>c": string, "l<c": string } |
+    { "l>=c": string, "l<c": string } |
+    { "l>c": string, "l<=c": string } |
+    { "true": string }
+)
