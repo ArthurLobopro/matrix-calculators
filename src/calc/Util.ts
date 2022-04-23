63ed04463e09ae8cd7ef065680a3b9ba2e69dc39
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-04-23 15:17:52 -0300

    chore: add typing from Util

--- src/calc/Util.ts
+++ src/calc/Util.ts
@@ -1,23 +1,14 @@
-/**
- * 
- * @param {number[][]} matrix 
- * @returns {boolean}
- */
-export function isSquareMatrix(matrix) {
+import { matrix } from "./types"
+
+export function isSquareMatrix(matrix: matrix): boolean {
     return matrix.every(line => line.length === matrix.length)
 }
 
-/**
- * 
- * @param {number[][]} matrix 
- * @param {number} order 
- * @returns {boolean}
- */
-export function isSquareOrder(matrix, order) {
+export function isSquareOrder(matrix: matrix, order: number): boolean {
     return isSquareMatrix(matrix) && matrix.length === order
 }
 
-export function isEqualFormat(m1, m2) {
+export function isEqualFormat(m1: matrix, m2: matrix): boolean {
     const columns_m1 = m1[0].length
     const lines_m1 = m1.length
     const columns_m2 = m2[0].length
